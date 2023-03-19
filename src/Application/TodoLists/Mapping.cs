4ca51d335ef0035af53f33b2b101d61b1aa44755
Igor Jigan <igor.jigan@ing.com>
2023-03-19 22:06:31 +0200

    Initial commit with basic structure.

---
+++ src/Application/TodoLists/Mapping.cs
@@ -0,0 +1,12 @@
+﻿using Cegeka.Auction.WebUI.Shared.TodoLists;
+
+namespace Cegeka.Auction.Application.TodoLists;
+
+public class Mapping : Profile
+{
+    public Mapping()
+    {
+        CreateMap<TodoList, TodoListDto>();
+        CreateMap<TodoItem, TodoItemDto>();
+    }
+}
---
+++ src/Infrastructure/Identity/ApplicationUser.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Cegeka.Auction.Infrastructure.Identity;
+
+public class ApplicationUser : IdentityUser
+{
+}
