95405d5687fdbb817c40cfacdbac881c4dbddf94
onelessangel <69592470+onelessangel@users.noreply.github.com>
2023-03-26 21:33:16 +0300

    Added Bid and BidDTO

---
+++ src/Domain/Entities/Bid.cs
@@ -0,0 +1,18 @@
+﻿using Cegeka.Auction.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cegeka.Auction.Domain.Entities;
+
+public class Bid : BaseAuditableEntity
+{
+    public int Id { get; set; }
+    public Guid PublicId { get; set; }
+    public int ItemId { get; set; }
+    public int UserId { get; set; }
+    public decimal Amount { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+}
