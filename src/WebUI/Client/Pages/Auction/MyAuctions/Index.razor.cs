cabca36d0c2804cfbfee6ee6ad8d1ec0052b1a30
onelessangel <69592470+onelessangel@users.noreply.github.com>
2023-04-11 15:28:55 +0300

    Updated Delete confirmation modal

--- src/WebUI/Client/Pages/Auction/MyAuctions/Index.razor.cs
+++ src/WebUI/Client/Pages/Auction/MyAuctions/Index.razor.cs
@@ -1,7 +1,5 @@
-﻿using Cegeka.Auction.WebUI.Shared.AccessControl;
-using Cegeka.Auction.WebUI.Shared.Auction;
+﻿using Cegeka.Auction.WebUI.Shared.Auction;
 using Microsoft.AspNetCore.Components;
-using System.Net.Http.Json;
 
 namespace Cegeka.Auction.WebUI.Client.Pages.Auction.MyAuctions;
 
@@ -12,9 +10,9 @@ public partial class Index
 
     public AuctionItemsVM? Model { get; set; }
 
-    public AuctionItemDTO _toDelete;
-
     public ConfirmationDialog ConfirmationDeleteDialog { get; set; }
+
+    private AuctionItemDTO _toDelete;
 
     protected override async Task OnInitializedAsync()
     {
@@ -29,9 +27,12 @@ public partial class Index
 
     protected async void OnConfirmationDeleteDialogClosed(bool arg)
     {
-        if (arg)
+        if (arg == false)
         {
-            await AuctionsClient.DeleteAuctionItemAsync(_toDelete.Id);
+            return;
         }
+        
+        await AuctionsClient.DeleteAuctionItemAsync(_toDelete.Id);
+        this.StateHasChanged();
     }
 }
