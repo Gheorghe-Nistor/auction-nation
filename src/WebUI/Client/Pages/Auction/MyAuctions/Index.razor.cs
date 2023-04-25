25f1b961f6122ad9133fc0b069401a779c54ad9b
onelessangel <69592470+onelessangel@users.noreply.github.com>
2023-04-25 16:40:57 +0300

    Added sort option
    Sorting by:
    - title;
    - category;
    - start date;
    - end date.

--- src/WebUI/Client/Pages/Auction/MyAuctions/Index.razor.cs
+++ src/WebUI/Client/Pages/Auction/MyAuctions/Index.razor.cs
@@ -1,6 +1,9 @@
 ﻿using Blazored.Toast.Services;
 using Cegeka.Auction.WebUI.Shared.Auction;
 using Microsoft.AspNetCore.Components;
+using System;
+using System.ComponentModel;
+using System.Reflection;
 
 namespace Cegeka.Auction.WebUI.Client.Pages.Auction.MyAuctions;
 
@@ -21,9 +24,76 @@ public partial class Index
 
     private AuctionItemDTO _toDelete;
 
+    public SortUtils Sorter { get; set; } = new SortUtils();
+
     protected override async Task OnInitializedAsync()
     {
         Model = await AuctionsClient.GetAuctionsAsync(); 
+    }
+
+    private void SortAuctions(string columnName)
+    {
+        string currentSortDirection;
+        Func<AuctionItemDTO, string> property;
+
+        switch(columnName)
+        {
+            case "Title":
+                currentSortDirection = Sorter.ByTitle;
+                property = a => a.Title;
+                break;
+
+            case "Category":
+                currentSortDirection = Sorter.ByCategory;
+                property = a => a.Category;
+                break;
+
+            case "Start Date":
+                currentSortDirection = Sorter.ByStartDate;
+                property = a => a.StartDate.ToString();
+                break;
+
+            case "End Date":
+                currentSortDirection = Sorter.ByEndDate;
+                property = a => a.EndDate.ToString();
+                break;
+
+            default:
+                throw new ArgumentException($"Invalid column name: {columnName}");
+        }
+
+        if (currentSortDirection == "asc")
+        {
+            Model.Auctions = Model.Auctions.OrderBy(property).ToList();
+            currentSortDirection = "desc";
+        }
+        else
+        {
+            Model.Auctions = Model.Auctions.OrderByDescending(property).ToList();
+            currentSortDirection = "asc";
+        }
+
+        switch (columnName)
+        {
+            case "Title":
+                Sorter.ByTitle = currentSortDirection;
+                break;
+
+            case "Category":
+                Sorter.ByCategory = currentSortDirection;
+                break;
+
+            case "Start Date":
+                Sorter.ByStartDate = currentSortDirection;
+                break;
+
+            case "End Date":
+                Sorter.ByEndDate = currentSortDirection;
+                break;
+
+            default:
+                throw new ArgumentException($"Invalid column name: {columnName}");
+        }
     }
 
     protected async Task ShowWarnings(AuctionItemDTO item, string auctionType)
@@ -43,8 +113,6 @@ public partial class Index
                 message = "Deleting this auction may result in a penalty or fee!";
                 toastService.ShowError(message);
             }
-
-            
         }
     }
 
